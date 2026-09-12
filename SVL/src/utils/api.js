a5664d77f8abef891029e1df5d3bf89d72be4b1a
Anand <110896646+anand22981@users.noreply.github.com>
2026-09-12 22:49:44 +0530

    fix oauth 7

--- SVL/src/utils/api.js
+++ SVL/src/utils/api.js
@@ -8,13 +8,13 @@ const api = axios.create({
 });
 
 // Attach session ID from sessionStorage to every request
-// api.interceptors.request.use((config) => {
-//   const sessionID = sessionStorage.getItem("sessionID");
-//   if (sessionID) {
-//     config.headers["X-Session-Id"] = sessionID;
-//   }
-//   return config;
-// });
+api.interceptors.request.use((config) => {
+  const sessionID = sessionStorage.getItem("sessionID");
+  if (sessionID) {
+    config.headers["X-Session-Id"] = sessionID;
+  }
+  return config;
+});
 
 export default api;
 export { API };
